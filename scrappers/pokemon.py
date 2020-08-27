065e9ec5bde66deba8f8c7cb487506f42b517af6
Guilherme Salustiano <jojo.guiss@hotmail.com>
2020-08-27 00:05:47 -0300

    Remove async in olds scrappers

--- scrappers/pokemon.py
+++ scrappers/pokemon.py
@@ -1,11 +1,10 @@
 from util import getImg
-import asyncio
 from uuid import uuid4
 import os
 
-async def main():
+def main():
     for id in range(1, 151):
-        folder = f"dataset/{id}"
+        folder = f"dataset/{id:03}"
         url = f"https://assets.pokemon.com/assets/cms2/img/pokedex/full/{id:03}.png"
 
         try:
@@ -14,7 +13,7 @@ async def main():
             pass
         filename = folder + f"/{uuid4()}.png"
         print(filename)
-        await getImg(url, filename)
+        getImg(url, filename)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
--- scrappers/util.py
+++ scrappers/util.py
@@ -1,12 +1,11 @@
 # Scrapper to oficial pokedex
 
 import requests
-import asyncio
 
-async def saveImg(img_data, filename):
+def saveImg(img_data, filename):
     with open(filename, mode='wb') as file:
         file.write(img_data)
 
-async def getImg(url, filename):
+def getImg(url, filename):
     img_data = requests.get(url).content
-    await saveImg(img_data, filename)
+    saveImg(img_data, filename)
