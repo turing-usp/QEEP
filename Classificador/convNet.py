d897248f4ad259e62a907f944c860c321a9e7099
Guilherme Salustiano <guissalustiano@gmail.com>
2021-02-19 16:10:57 -0300

    Add main to convNet

--- Classificador/convNet.py
+++ Classificador/convNet.py
@@ -119,28 +119,27 @@ def visualize_model(model, num_images=6):
         model.train(mode=was_training)
 
 
-# def main():
-# %%
-model_ft = models.resnet18(pretrained=True)
-num_ftrs = model_ft.fc.in_features
-model_ft.fc = nn.Linear(num_ftrs, len(pokeDataset['full'].classes))
+def main():
+    model_ft = models.resnet18(pretrained=True)
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, len(pokeDataset['full'].classes))
 
-model_ft = model_ft.to(device)
+    model_ft = model_ft.to(device)
 
-criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss()
 
-# Observe that all parameters are being optimized
-optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
 
-# Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(
-    optimizer_ft, step_size=7, gamma=0.1)
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(
+        optimizer_ft, step_size=7, gamma=0.1)
 
-model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=25)
+    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                           num_epochs=25)
 
-visualize_model(model_ft)
+    visualize_model(model_ft)
 
 
-# if __name__ == "__main__":
-# main()
+if __name__ == "__main__":
+    main()
