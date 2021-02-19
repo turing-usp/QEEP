0f8bccc2fa5eb8f2ec2b61270104a33404ecf02a
Guilherme Salustiano <guissalustiano@gmail.com>
2021-02-18 22:04:34 -0300

    Update dataset to map

--- Classificador/dataset.py
+++ Classificador/dataset.py
@@ -1,3 +1,4 @@
+from itertools import chain
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,12 +12,34 @@ data_transform = transforms.Compose([
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
 ])
-pokeDataset = datasets.ImageFolder(root=DATASET_PATH,
-                                   transform=data_transform)
 
-dataloader = torch.utils.data.DataLoader(pokeDataset,
+_pokeDataset_full = datasets.ImageFolder(root=DATASET_PATH,
+                                         transform=data_transform)
+
+datasetLen = {
+    'full': len(_pokeDataset_full)
+}
+datasetLen['train'] = int(0.7*datasetLen['full'])
+datasetLen['val'] = datasetLen['full'] - datasetLen['train']
+
+[_pokeDataset_train, _pokeDataset_val] = torch.utils.data.random_split(
+    _pokeDataset_full, [datasetLen['train'], datasetLen['val']])
+
+pokeDataset = {
+    'full': _pokeDataset_full,
+    'train': _pokeDataset_train,
+    'val': _pokeDataset_val
+}
+
+pokeLoader = {
+    'train': torch.utils.data.DataLoader(pokeDataset['train'],
                                          batch_size=4, shuffle=True,
-                                         num_workers=4)
+                                         num_workers=4),
+    'val': torch.utils.data.DataLoader(pokeDataset['val'],
+                                       batch_size=4, shuffle=True,
+                                       num_workers=4)
+}
+pokeLoader['full'] = chain(pokeLoader['train'], pokeLoader['val'])
 
 
 def imshow(inp, title=None):
@@ -31,7 +54,7 @@ def imshow(inp, title=None):
 
 if __name__ == "__main__":
     # Get a batch of training data
-    for inputs, classes in dataloader:
+    for inputs, classes in (pokeLoader['full']):
         out = torchvision.utils.make_grid(inputs)
-        imshow(out, title=[pokeDataset.classes[x] for x in classes])
+        imshow(out, title=[_pokeDataset_full.classes[x] for x in classes])
         plt.show()
