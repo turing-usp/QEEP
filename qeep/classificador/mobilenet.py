a509f1b9b6d58fabfc131b4f1558c45394a58037
Noel <noel.eliezer@pm.me>
2021-03-09 20:07:40 -0300

    MobileNet class for training and prediction

--- qeep/classificador/mobilenet.py
+++ qeep/classificador/mobilenet.py
@@ -2,42 +2,50 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
-from ..util.storage import saveModel
-from ..util.train import trainModel
+import ..util.train as train
+from ..util.storage import saveModel, loadModel
 from .dataset import loadSplitedLoader
 
+class MobileNet():
+    def __init__(self):
+        pass
 
-def createMobilenet(output_size, input_size, device):
+    def loadDataset(self, *args, **kwargs):
+        *self.data_loaders, self.dataset = loadSplitedLoader(*args, **kwargs)
+        self.classes_len = len(dataset.classes)
 
-    model = torch.hub.load('pytorch/vision:v0.6.0',
-                           'mobilenet_v2', pretrained=True)
-    model.eval()
+    def trainModel(self, batch_size=4, criterion=None, optimizer=None, scheduler=None,
+                   epochs=25, learning_rate=0.001):
+        if criterion is None:
+            criterion = nn.CrossEntropyLoss()
+        if optimizer is None:
+            optimizer = optim.SGD(self.model.parameters(), lr=learning_rate, momentum=0.9)
+        if scheduler is None:
+            scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
-    num_ftrs = model.fc.in_features
+        train.trainModel(self.model, self.data_loaders, criterion, optimizer, scheduler, epochs)
 
-    model.fc = nn.Linear(num_ftrs, output_size)
+    def createMobilenet(data_loaders=None, dataset=None):
+        instance = MobileNet()
+        if dataset is None:
+            instance.LoadDataset()
+        else:
+            instance.data_loaders = data_loaders
+            instance.dataset = dataset
+            instance.classes_len = len(dataset.classes)
 
-    model = model.to(device)
+        instance.model = torch.hub.load('pytorch/vision:v0.6.0',
+                                        'mobilenet_v2', pretrained=True)
 
-    return model
+        num_ftrs = instance.model.fc.in_features
 
+        instance.model.fc = nn.Linear(num_ftrs, instance.classes_len)
 
-if __name__ == "__main__":
-    *data_loaders, dataset = loadSplitedLoader()
-    classes_len = len(dataset.classes)
-
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = createMobilenet(classes_len, 256*256, device)
-
-    criterion = nn.CrossEntropyLoss()
-
-    optimizer_ft = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        return instance
 
 
-    exp_lr_scheduler = lr_scheduler.StepLR(
-        optimizer_ft, step_size=7, gamma=0.1)
-
-    model = trainModel(model, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=25)
-
-    saveModel("mobilenet", model, path="../models")
+    def loadPretrained(model="mobilenet", file: str = "mobilenet_weights.pkl", drive: bool = True,
+                       url: str ="https://drive.google.com/uc?export=download&id=1yC0qK0gVX5sc6GTpBPBupH3TSFssLkqS")
+        instance = MobileNet()
+        instance.model = loadModel(model, file, drive, url)
+        return instance
