7947ebb903043c0572ff50dcf9f847c589c407af
Guilherme Salustiano <guissalustiano@gmail.com>
2021-03-10 16:05:49 -0300

    Refactory mobilenet.py

--- qeep/classificador/mobilenet.py
+++ qeep/classificador/mobilenet.py
@@ -1,62 +1,15 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
-from ..util import train
-from ..util.storage import saveModel, loadModel
-from .dataset import loadSplitedLoader
+from ..util.model_validation import ModelValidation
+from ..util.model_dataset import ModelDataset
+from ..util.model_storage import ModelStorage
 
 
-class MobileNet():
-    def __init__(self):
-        pass
+class MobileNet(ModelValidation, ModelStorage, ModelDataset):
+    def __init__(self, output_size: int):
+        self.model = torch.hub.load('pytorch/vision:v0.6.0',
+                                    'mobilenet_v2', pretrained=True)
 
-    def loadDataset(self, *args, **kwargs):
-        *self.data_loaders, self.dataset = loadSplitedLoader(*args, **kwargs)
-        self.classes_len = len(self.dataset.classes)
+        num_ftrs = self.model.fc.in_features
 
-    def trainModel(self, batch_size=4, criterion=None, optimizer=None, scheduler=None,
-                   epochs=25, learning_rate=0.001):
-        if criterion is None:
-            criterion = nn.CrossEntropyLoss()
-        if optimizer is None:
-            optimizer = optim.SGD(self.model.parameters(), lr=learning_rate, momentum=0.9)
-        if scheduler is None:
-            scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-
-        train.trainModel(self.model, self.data_loaders, criterion, optimizer, scheduler, epochs)
-
-    def createMobilenet(data_loaders=None, dataset=None):
-        instance = MobileNet()
-        if dataset is None:
-            instance.LoadDataset()
-        else:
-            instance.data_loaders = data_loaders
-            instance.dataset = dataset
-            instance.classes_len = len(dataset.classes)
-
-        instance.model = torch.hub.load('pytorch/vision:v0.6.0',
-                                        'mobilenet_v2', pretrained=True)
-
-        num_ftrs = instance.model.fc.in_features
-
-        instance.model.fc = nn.Linear(num_ftrs, instance.classes_len)
-
-        return instance
-
-    def loadPretrained(model="mobilenet", file: str = "mobilenet_weights.pkl", drive: bool = True,
-                       url: str ="https://drive.google.com/uc?export=download&id=1yC0qK0gVX5sc6GTpBPBupH3TSFssLkqS"):
-        instance = MobileNet()
-        instance.model = loadModel(model, file, drive, url)
-        return instance
-
-    def save(self, name: str, model: torch.nn.Module = None, path: str = ""):
-        """
-        Salva um PyTorch model, no path e nome desejados, com extensão .pkl
-        ---------------
-        Argumentos:
-            - model: PyTorch model (torch.nn.Module)
-            - path: string contendo o path para salvar o modelo
-            - name: string contendo o nome do arquivo salvo
-        """
-        saveModel(name, model, path)
+        self.model.fc = nn.Linear(num_ftrs, output_size)
