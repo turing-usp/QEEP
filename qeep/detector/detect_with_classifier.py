8c09eef94e42bcad664c47ab9039e158d5a744f3
Rafael <rafaelcoelho@usp.br>
2021-03-11 23:41:33 -0300

    Atualiza detect_with_classifier.py O arquivo funcao.py foi alterado para detect_with_classifier.py, contendo a rotina do detector

--- qeep/detector/detect_with_classifier.py
+++ qeep/detector/detect_with_classifier.py
@@ -1,14 +1,13 @@
-# https://www.pyimagesearch.com/2020/06/22/turning-any-cnn-image-classifier-into-an-object-detector-with-keras-tensorflow-and-opencv/
+# Referência:
 # Turning any CNN image classifier into an object detector with Keras, TensorFlow, and OpenCV
+# https://www.pyimagesearch.com/2020/06/22/turning-any-cnn-image-classifier-into-an-object-detector-with-keras-tensorflow-and-opencv/
 
-# Conversão do classificador em um detector
+# Conversão de uma CNN classificadora em um detector de objetos com Pytorch
 
-# import the necessary packages
-from tensorflow.keras.applications.resnet import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-from imutils.object_detection import non_max_suppression
+from detection_helpers import img_to_array
 from detection_helpers import sliding_window
 from detection_helpers import image_pyramid
+from imutils.object_detection import non_max_suppression
 import json
 import numpy as np
 import argparse
@@ -16,178 +15,192 @@ import imutils
 import time
 import cv2
 import torch
-# --------
-
-# some_file.py
 import sys
-# insert at 1, 0 is the script path (or '' in REPL)
+
+# Adiciona o path dos outros módulos
 sys.path.insert(1, '..')
 #from classificador.classes import loadClasses
 from util.storage import loadModel
 from util.validation import getPredictions
 
-#----------
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to the input image")
-ap.add_argument("-s", "--size", type=str, default="(200, 150)",
-	help="ROI size (in pixels)")
-ap.add_argument("-c", "--min-conf", type=float, default=0.9,
-	help="minimum probability to filter weak detections")
-ap.add_argument("-v", "--visualize", type=int, default=-1,
-	help="whether or not to show extra visualizations for debugging")
-args = vars(ap.parse_args())
+def get_rois(original_img, PYR_SCALE, WIN_STEP, ROI_SIZE, visualize):
 
-# initialize variables used for the object detection procedure
-WIDTH = 600
-PYR_SCALE = 1.5
-WIN_STEP = 16
-ROI_SIZE = eval(args["size"])
-INPUT_SIZE = (224, 224)
+    # Inicializa a pirâmide da imagem
+    pyramid = image_pyramid(original_img, scale=PYR_SCALE, minSize=ROI_SIZE)
+    # Inicializa a lista de ROIs (Regiões de Interesse) geradas pela 
+    # pirâmide e pela sliding window, e a lista de coordenadas (x, y), 
+    # para guardar a posição de cada ROI na imagem original
+    rois = []
+    locs = []
+    # Tempo inicial do pré-processamento da imagem original
+    start = time.time()
 
-# load our network weights from disk
-print("[INFO] loading network...")
-model = loadModel(model="shufle", file="weights.pkl",drive=False)
-#Muda o modelo para evaluation mode
+    # Dimensões da imagem original
+    (H, W) = original_img.shape[:2]
 
-# load the input image from disk, resize it such that it has the
-# has the supplied width, and then grab its dimensions
-orig = cv2.imread(args["image"])
-orig = imutils.resize(orig, width=WIDTH)
-(H, W) = orig.shape[:2]
+    # Aplica o algoritmo da pirâmide sobre a imagem
+    for image in pyramid:
+        # Obtém o fator de escala entre a imagem original e
+        # a camada atual da pirâmide
+        scale = W / float(image.shape[1])
+        # Para cada camada da pirâmide, aplica o algoritmo de sliding window
+        for (x, y, original_roi) in sliding_window(image, WIN_STEP, ROI_SIZE):
+            # Reescala as coordenadas (x, y) da ROI com respeito às
+            # dimensões da imagem original
+            x = int(x * scale)
+            y = int(y * scale)
+            w = int(ROI_SIZE[0] * scale)
+            h = int(ROI_SIZE[1] * scale)
+            # Transforma a ROI (PIL Image) em array para a classificação
+            roi = img_to_array(original_roi)
+            # Atualiza a lista de ROIS e a lista de coordenadas
+            rois.append(roi)
+            locs.append((x, y, x + w, y + h))
 
-# initialize the image pyramid
-pyramid = image_pyramid(orig, scale=PYR_SCALE, minSize=ROI_SIZE)
-# initialize two lists, one to hold the ROIs generated from the image
-# pyramid and sliding window, and another list used to store the
-# (x, y)-coordinates of where the ROI was in the original image
-rois = []
-locs = []
-# time how long it takes to loop over the image pyramid layers and
-# sliding window locations
-start = time.time()
+            # Checa se as sliding windows devem ser mostradas
+            if visualize > 0:
+                # Clona a imagem original e então desenha as bounding boxes
+                # representando a ROI atual
+                clone = original_img.copy()
+                cv2.rectangle(clone, (x, y), (x + w, y + h),
+                    (0, 255, 0), 2)
+                # Mostra a ROI atual na tela
+                cv2.imshow("Sliding Window", clone)
+                cv2.imshow("ROI", original_roi)
+                cv2.waitKey(0)
 
-# loop over the image pyramid
-for image in pyramid:
-	# determine the scale factor between the *original* image
-	# dimensions and the *current* layer of the pyramid
-	scale = W / float(image.shape[1])
-	# for each layer of the image pyramid, loop over the sliding
-	# window locations
-	for (x, y, roiOrig) in sliding_window(image, WIN_STEP, ROI_SIZE):
-		# scale the (x, y)-coordinates of the ROI with respect to the
-		# *original* image dimensions
-		x = int(x * scale)
-		y = int(y * scale)
-		w = int(ROI_SIZE[0] * scale)
-		h = int(ROI_SIZE[1] * scale)
-		# take the ROI and preprocess it so we can later classify
-		# the region using Keras/TensorFlow
-		roi = cv2.resize(roiOrig, INPUT_SIZE)
-		roi = img_to_array(roi)
-		roi = preprocess_input(roi)
-		# update our list of ROIs and associated coordinates
-		rois.append(roi)
-		locs.append((x, y, x + w, y + h))
+    # Fecha as sliding windows mostradas
+    if visualize > 0:
+        cv2.destroyAllWindows()
 
-		# check to see if we are visualizing each of the sliding
-		# windows in the image pyramid
-		if args["visualize"] > 0:
-			# clone the original image and then draw a bounding box
-			# surrounding the current region
-			clone = orig.copy()
-			cv2.rectangle(clone, (x, y), (x + w, y + h),
-				(0, 255, 0), 2)
-			# show the visualization and current ROI
-			cv2.imshow("Visualization", clone)
-			cv2.imshow("ROI", roiOrig)
-			cv2.waitKey(0)
+    # Tempo final do pré-processamento da imagem original
+    end = time.time()
+    # Duração do pré-processamento da imagem original
+    print("[INFO] O pré-processamento da imagem durou {:.5f} segundos".format(
+        end - start))
+    
+    return rois, locs
 
-# show how long it took to loop over the image pyramid layers and
-# sliding window locations
-end = time.time()
-print("[INFO] looping over pyramid/windows took {:.5f} seconds".format(
-	end - start))
-# convert the ROIs to a NumPy array
-rois = np.array(rois,dtype="int8")
-# classify each of the proposal ROIs using ResNet and then show how
-# long the classifications took
+def classify_rois(model, rois, classes):
 
-print("[INFO] classifying ROIs...")
-start = time.time()
-with open("classes.json", mode="r") as f:
-	classes = json.load(f)
-preds = getPredictions(model,class_names=classes,image_arrays=rois,quiet=True)
-preds = list(map(lambda x : x.detach().cpu().numpy(), preds))
-preds = np.array(preds)
-preds = preds[:,:,:151]
-end = time.time()
-print("[INFO] classifying ROIs took {:.5f} seconds".format(
-	end - start))
-results = []
-#_, saida = torch.max(preds[130], 1)
-#print(saida)
-for pred in preds:
-	top_indice = pred.argsort()[0,-1]
-	result = (top_indice,classes[top_indice],pred[0][top_indice])
-	results.append(result)
-preds = results
+    print("[INFO] Classificando as ROIs")
+    # Tempo inicial da classificação das ROIs
+    start = time.time()
+
+    # Classifica cada uma das ROIs utilizando o modelo
+    preds = getPredictions(model, class_names = classes, image_arrays = rois, quiet = True)
+    # Transforma as predições em um array
+    preds = list(map(lambda x : x.detach().cpu().numpy(), preds))
+    preds = np.array(preds)
+    # Seleciona apenas as primeiras 151 classes de saída (erro no modelo) 
+    preds = preds[:,:,:len(classes)]
+
+    # Tempo final da classificação das ROIs
+    end = time.time()
+    print("[INFO] A classificalão das ROIs durou {:.5f} segundos".format(
+        end - start))
+
+    # Inicializa a lista de predições das ROIs
+    results = []
+    # Itera sobre todas as predições
+    for pred in preds:
+        # Seleciona o índice da classe mais provável
+        top_indice = pred.argsort()[0,-1]
+        # Cria uma tupla contendo top_indice, a classe e a probabilidade
+        result = (top_indice, classes[top_indice], pred[0][top_indice])
+        # Adiciona a tupla criada na lista de resultados
+        results.append(result)
+
+    return results
 
 
-# decode the predictions and initialize a dictionary which maps class
-# labels (keys) to any ROIs associated with that label (values)
-# preds = imagenet_utils.decode_predictions(preds, top=1)
-labels = {}
+def filter_detections(original_img, preds, locs, min_conf, visualize):
 
-# loop over the predictions
-for (i, p) in enumerate(preds):
-	# grab the prediction information for the current ROI
-	(imagenetID, label, prob) = p
-	# filter out weak detections by ensuring the predicted probability
-	# is greater than the minimum probability
-	if prob >= args["min_conf"]:
-		# grab the bounding box associated with the prediction and
-		# convert the coordinates
-		box = locs[i]
-		# grab the list of predictions for the label and add the
-		# bounding box and probability to the list
-		L = labels.get(label, [])
-		L.append((box, prob))
-		labels[label] = L
+    # Inicializa dicionário de predições separado por classes
 
-# loop over the labels for each of detected objects in the image
-for label in labels.keys():
-	# clone the original image so that we can draw on it
-	print("[INFO] showing results for '{}'".format(label))
-	clone = orig.copy()
-	# loop over all bounding boxes for the current label
-	for (box, prob) in labels[label]:
-		# draw the bounding box on the image
-		(startX, startY, endX, endY) = box
-		cv2.rectangle(clone, (startX, startY), (endX, endY),
-			(0, 255, 0), 2)
-	# show the results *before* applying non-maxima suppression, then
-	# clone the image again so we can display the results *after*
-	# applying non-maxima suppression
-	cv2.imshow("Before", clone)
-	clone = orig.copy()
+    labels = {}
 
-	# extract the bounding boxes and associated prediction
-	# probabilities, then apply non-maxima suppression
-	boxes = np.array([p[0] for p in labels[label]])
-	proba = np.array([p[1] for p in labels[label]])
-	boxes = non_max_suppression(boxes, proba)
-	# loop over all bounding boxes that were kept after applying
-	# non-maxima suppression
-	for (startX, startY, endX, endY) in boxes:
-		# draw the bounding box and label on the image
-		cv2.rectangle(clone, (startX, startY), (endX, endY),
-			(0, 255, 0), 2)
-		y = startY - 10 if startY - 10 > 10 else startY + 10
-		cv2.putText(clone, label, (startX, y),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
-	# show the output after apply non-maxima suppression
-	cv2.imshow("After", clone)
-	cv2.waitKey(0)
+    # Itera sobre todas as predições de cada ROI
+    for (i, p) in enumerate(preds):
+        # Separa as informações da ROI atual
+        (imagenetID, label, prob) = p
+        # Filtra as detecções com baixa prioridade de acordo com min_conf
+        if prob >= min_conf:
+            # Seleciona a bounding box da predição
+            box = locs[i]
+            # Caso a chave label exista no dicionário labels, guarda seu valor em L
+		    # Se não, guarda uma lista vazia em L
+            L = labels.get(label, [])
+            # Adiciona a bounding box e sua probabilidade no dicionário de labels
+            L.append((box, prob))
+            labels[label] = L
 
+    # Itera sobre cada uma das classes detectadas
+    for label in labels.keys():
+        print("[INFO] Apresentando os resutados para '{}'".format(label))
+        
+        # Checa se as bounding boxes encontradas devem ser mostradas
+        if visualize > 0:
+            # Clona a imagem original para desenhar as bounding boxes
+            clone = original_img.copy()
+            # Itera sobre todas as bounding boxes da classe atual
+            for (box, prob) in labels[label]:
+                # Desenha as bounding bouxes na imagem
+                (startX, startY, endX, endY) = box
+                cv2.rectangle(clone, (startX, startY), (endX, endY),
+                    (0, 255, 0), 2)
+            cv2.imshow("Debug", clone)
+
+        # Clona a imagem original para desenhar as bounding boxes
+        clone = original_img.copy()
+        # Aplica a técnica de non-maxima supression nas predições da classe atual
+        boxes = np.array([p[0] for p in labels[label]])
+        proba = np.array([p[1] for p in labels[label]])
+        boxes = non_max_suppression(boxes, proba)
+        # Itera sobre as bounding boxes remanescenes 
+        for (startX, startY, endX, endY) in boxes:
+            # Desenha as bounding bouxes na imagem
+            cv2.rectangle(clone, (startX, startY), (endX, endY),
+                (0, 255, 0), 2)
+            y = startY - 10 if startY - 10 > 10 else startY + 10
+            cv2.putText(clone, label, (startX, y),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
+        cv2.imshow("Predicoes", clone)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+
+    # Arg parser
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True,
+        help="path to the input image")
+    ap.add_argument("-s", "--size", type=str, default="(200, 150)",
+        help="ROI size (in pixels)")
+    ap.add_argument("-c", "--min-conf", type=float, default=0.9,
+        help="minimum probability to filter weak detections")
+    ap.add_argument("-v", "--visualize", type=int, default=-1,
+        help="whether or not to show extra visualizations for debugging")
+    args = vars(ap.parse_args())
+
+    with open("classes.json", mode="r") as f:
+        classes = json.load(f)
+
+    # Parâmetros
+    WIDTH = 600
+    PYR_SCALE = 1.5
+    WIN_STEP = 16
+    ROI_SIZE = eval(args["size"])
+
+    # Carregamento do modelo
+    print("[INFO] loading network...")
+    model = loadModel(model = "mobilenet")
+
+    # Carrega a imagem selecionada
+    image = cv2.imread(args["image"])
+    image = imutils.resize(image, width = WIDTH)
+
+    # Roda o detector
+    rois, locs = get_rois(image, PYR_SCALE, WIN_STEP, ROI_SIZE, args["visualize"])
+    predictions = classify_rois(model, rois, classes)
+    filter_detections(image, predictions, locs, args["min_conf"], args["visualize"])
