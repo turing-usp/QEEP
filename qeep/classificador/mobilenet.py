Add softmax in model and fix train

qeep/classificador/mobilenet.py
@@ -4,15 +4,15 @@ from ..util.model import ModelUtil
 
 
 class MobileNet(ModelUtil):
-    def __init__(self, output_size: int):
+    def __init__(self, output_size: int, freeze: bool = True):
         model = torch.hub.load('pytorch/vision:v0.6.0',
                                'mobilenet_v2', pretrained=True)
-
+        if freeze:
+            for param in model.parameters():
+                param.requires_grad = False
         num_ftrs = model.classifier[-1].in_features
         model.classifier[-1] = nn.Linear(num_ftrs, output_size)
-
-        # model_output = model.classifier[-1].out_features
-        # model.classifier.append(nn.Softmax(output_size))
+        model.classifier.add_module('2', nn.Softmax())
 
         self.model = model.to(self.device)
 

qeep/util/model.py
@@ -38,7 +38,7 @@ class ModelUtil():
     def trainModel(self, criterion=None, optimizer=None, scheduler: optim.lr_scheduler.StepLR = None,
                    epochs: int = 25, learning_rate: float = 0.001):
         if criterion is None:
-            criterion = nn.CrossEntropyLoss()
+            criterion = nn.NLLLoss() # change loss
         if optimizer is None:
             optimizer = optim.SGD(self.model.parameters(),
                                   lr=learning_rate, momentum=0.9)
@@ -46,43 +46,85 @@ class ModelUtil():
             scheduler = optim.lr_scheduler.StepLR(
                 optimizer, step_size=7, gamma=0.1)
 
+        since = time.time()
+
+        best_model_wts = copy.deepcopy(self.model.state_dict())
+        best_acc = 0.0
+
+        dataloaders = {
+            'train': self.dataloaders[0],
+            'val': self.dataloaders[-1]
+        }
+        dataset_sizes = {
+            'train': len(self.dataset_splited[0]),
+            'val': len(self.dataset_splited[-1])
+        }
+
         for epoch in range(epochs):
             print('Epoch {}/{}'.format(epoch, epochs - 1))
             print('-' * 10)
 
-            # Train
-            self.model.train()
-            running_loss = 0.0
-            running_corrects = 0
-            # Iterate over data.
-            for inputs, labels in self.dataloaders[0]:
-                inputs = inputs.to(self.device)
-                labels = labels.to(self.device)
+            # Each epoch has a training and validation phase
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    self.model.train()  # Set model to training mode
+                else:
+                    self.model.eval()   # Set model to evaluate mode
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
+                running_loss = 0.0
+                running_corrects = 0
 
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(True):
-                    outputs = self.model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
-                    loss.backward()
-                    optimizer.step()
+                # Iterate over data.
+                for inputs, labels in dataloaders[phase]:
+                    inputs = inputs.to(self.device)
+                    labels = labels.to(self.device)
 
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
 
-                scheduler.step()
+                    # forward
+                    # track history if only in train
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = self.model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
 
-            epoch_loss = running_loss / len(self.dataloaders[0])
-            epoch_acc = running_corrects.double() / len(self.dataloaders[0])
+                        # backward + optimize only if in training phase
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
 
-            print(f'Train Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
-            print(f'Validation Accuracy: {self.modelAccuracy()}')
+                    # statistics
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+                if phase == 'train':
+                    scheduler.step()
+
+                epoch_loss = running_loss / dataset_sizes[phase]
+                epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+                print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                    phase, epoch_loss, epoch_acc))
+
+                # deep copy the model
+                if phase == 'val' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    best_model_wts = copy.deepcopy(self.model.state_dict())
+
             print()
+
+        time_elapsed = time.time() - since
+        print('Training complete in {:.0f}m {:.0f}s'.format(
+            time_elapsed // 60, time_elapsed % 60))
+        print('Best val Acc: {:4f}'.format(best_acc))
+
+        # load best model weights
+        self.model.load_state_dict(best_model_wts)
+        return self.model
+
+        print(f'Train Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+        print(f'Validation Accuracy: {self.modelAccuracy()}')
+        print()
 
     def modelAccuracy(self, dataloader: torch.utils.data.DataLoader = None):
         """