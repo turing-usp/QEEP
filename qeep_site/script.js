5acb4346114f5aadfec62ed1bb07bbf512b10f06
Guilherme Salustiano <guissalustiano@gmail.com>
2021-05-12 12:38:37 -0300

    refa: create use functions

--- qeep_site/script.js
+++ qeep_site/script.js
@@ -4,11 +4,16 @@ const pokefile = document.getElementById("pokefile");
 const pokeinput = document.getElementById("image-input");
 
 pokefile.parentElement.onclick = () => pokefile.click();
-pokefile.onchange = (e) => {
-  const [file] = pokefile.files;
-  if (file) {
+
+const showInputFile = (file) => {
     pokeinput.src = URL.createObjectURL(file);
     pokeinput.style.display = "block";
     pokeinput.previousElementSibling.style.display = "none";
+}
+
+pokefile.onchange = (e) => {
+  const [file] = pokefile.files;
+  if (file) {
+    showInputFile(file);
   }
 };
