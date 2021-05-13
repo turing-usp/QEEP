990a7ad4c19f36f644cca0ba074e1625ecbe6265
Guilherme Salustiano <guissalustiano@gmail.com>
2021-05-12 21:05:24 -0300

    send image to lambda

--- qeep_site/script.js
+++ qeep_site/script.js
@@ -1,5 +1,7 @@
 console.log("Script rodando");
 
+const QEEP_URL = "https://mcdxkzn708.execute-api.sa-east-1.amazonaws.com/qeep"
+
 const pokefile = document.getElementById("pokefile");
 const pokeinput = document.getElementById("image-input");
 
@@ -11,9 +13,34 @@ const showInputFile = (file) => {
     pokeinput.previousElementSibling.style.display = "none";
 }
 
+const uploadFile = (file) => {
+  console.log(`sending: ${file}`)
+  return fetch(QEEP_URL, {
+    method: 'POST',
+     headers: {
+      "Content-Type": file.type,
+     },
+      body: file
+  }).then(
+    response => response.json()
+  ).then(
+    success => {
+      console.log(success);
+      return success.body.outputlink;
+    }
+  ).catch(
+    error => console.log(error)
+  );
+}
+
 pokefile.onchange = (e) => {
   const [file] = pokefile.files;
   if (file) {
     showInputFile(file);
+    uploadFile(
+      file
+    ).then(
+      outputlink => console.log("agora é só fazer esperar até o ", outputlink)
+    );
   }
 };
